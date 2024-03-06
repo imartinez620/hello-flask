0c1cad712379b4660454f7dc88787f7da8800e61
imartinez620 <info.imartinez@gmail.com>
2024-03-06 18:05:53 +0100

    get

--- app.py
+++ app.py
@@ -33,7 +33,8 @@ class GuideSchema(ma.Schema):
 guide_schema = GuideSchema()
 guides_schema = GuideSchema(many=True)
 
-# End point to create a new guide
+
+# Endpoint to create a new guide
 @app.route('/guide', methods=['POST'])
 def add_guide():
 	title = request.json['title']
@@ -48,6 +49,20 @@ def add_guide():
 
 	return guide_schema.jsonify(guide)
 
+
+# Endpoint to query all guides
+@app.route('/guides', methods=['GET'])
+def get_guides():
+    all_guides = Guide.query.all()
+    result = guides_schema.dump(all_guides)
+    return jsonify(result)
+
+
+# Endpoint for quering a single guide
+@app.route('/guide/<id>', methods=['get'])
+def get_guide(id):
+	guide = Guide.query.get(id)
+	return guide_schema.jsonify(guide)
 
 if __name__ == '__main__':
 	app.run(debug=True)
